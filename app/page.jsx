2d2fc38153c9b882e8d72aace74793154eabe2f5
Sutichai <kazychild@hotmail.co.th>
2025-01-11 14:36:46 +0700

    refact: reset entire project

---
+++ app/page.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod
+        evenietautem iure, consequuntur eos odit ad alias, corporis tempore
+        sunt, cumvelit! Accusantium excepturi nesciunt quod, adipisci ab
+        voluptatum undeculpa, praesentium dicta eius dolorem, voluptas ipsam
+        porro modireiciendis necessitatibus doloremque quis. Sunt ea dicta
+        harum, remquisquam natus.
+      </p>
+    </div>
+  );
+};
+
+export default Homepage;
